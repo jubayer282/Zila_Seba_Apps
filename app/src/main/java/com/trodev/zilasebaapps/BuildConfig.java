c3143fc7611c9b60f70986529681873ab3a34fe0
zobayerdev <zobayer.dev@gmail.com>
2023-05-23 22:02:24 +0600

    new system update

---
+++ app/src/main/java/com/trodev/zilasebaapps/BuildConfig.java
@@ -0,0 +1,9 @@
+package com.trodev.zilasebaapps;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+    public static final String APPLICATION_ID = "com.trodev.medicarepro";
+    public static final String BUILD_TYPE = "debug";
+    public static final int VERSION_CODE = 1;
+    public static final String VERSION_NAME = "1.0";
+}
